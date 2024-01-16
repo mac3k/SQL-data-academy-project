c59365bff50bf933fc4498901ff3c155c1c70f46
Marcel Sufcak <153240524+mac3k@users.noreply.github.com>
2024-01-16 11:10:12 +0100

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,11 +1,11 @@
--- 2) How much can one buy in terms of liters of milk and kilograms of bread for the first and last comparable periods in the available data on prices and wages?
+-- Task 2: How much can one buy in terms of liters of milk and kilograms of bread for the first and last comparable periods in the available data on prices and wages?
 
 -- Find out the first and last year in available data:
 SELECT MIN (year)
 	,MAX (year)
 FROM t_marcel_sufcak_project_SQL_primary_final;
 
--- Employees from different industries can buy (in 2006 and 2018)
+-- Employees from different industries can buy (in 2006 and 2018):
 SELECT *
 	,ROUND(ind_avg_payroll_value / cat_avg_value_price, 0) AS available_entity
 FROM t_marcel_sufcak_project_SQL_primary_final
@@ -13,8 +13,7 @@ WHERE year IN (2006, 2018)
 	AND cat_code IN (111301,114201)
 ORDER BY year, cat_code;
 
-
--- Employees from overall industries can buy (in 2006 and 2018)
+-- Employees from overall industries can buy (in 2006 and 2018):
 SELECT `year` 
 	,ROUND(AVG(ind_avg_payroll_value), 2) AS avg_payroll_all_industries
 	,ind_pay_unit 
@@ -29,4 +28,3 @@ WHERE year IN (2006, 2018)
 	AND cat_code IN (111301,114201)
 GROUP BY year, cat_code
 ORDER BY year, cat_code;
-
