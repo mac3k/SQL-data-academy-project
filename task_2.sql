e284cfebc0c6464a5448b52a29929a018110aa89
Marcel Sufcak <153240524+mac3k@users.noreply.github.com>
2024-01-24 10:19:07 +0100

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,30 +1,37 @@
 -- Task 2: How much can one buy in terms of liters of milk and kilograms of bread for the first and last comparable periods in the available data on prices and wages?
 
--- Find out the first and last year in available data:
-SELECT MIN (year)
-	,MAX (year)
-FROM t_marcel_sufcak_project_SQL_primary_final;
+-- Find out the first and last year in available data and what employees from different industries can buy (in min [2006] and max [2018] years):
+WITH min_max_years AS (
+	SELECT 
+		MIN (year) AS min_year,
+		MAX (year) AS max_year
+	FROM t_marcel_sufcak_project_SQL_primary_final)
+SELECT base.*,
+	ROUND(industry_avg_payroll_value / category_avg_value_price, 0) AS available_entity
+FROM t_marcel_sufcak_project_SQL_primary_final base
+JOIN min_max_years m
+WHERE year IN (m.min_year, m.max_year)
+	AND category_code IN (111301,114201)
+ORDER BY year, category_code;
 
--- Employees from different industries can buy (in 2006 and 2018):
-SELECT *
-	,ROUND(ind_avg_payroll_value / cat_avg_value_price, 0) AS available_entity
+-- Employees from overall industries can buy (in min [2006] and max [2018] years):
+WITH min_max_years AS (
+	SELECT 
+		MIN (year) AS min_year,
+		MAX (year) AS max_year
+	FROM t_marcel_sufcak_project_SQL_primary_final) 
+SELECT `year`, 
+	ROUND(AVG(industry_avg_payroll_value), 2) AS avg_payroll_all_industries,
+	industry_pay_unit,
+	category_name,
+	category_code,
+	category_avg_value_price,
+	category_price_unit,
+	ROUND(AVG(industry_avg_payroll_value) / AVG(category_avg_value_price), 0) AS available_entity,
+	CONCAT('A person with an average salary of ', ROUND(AVG(industry_avg_payroll_value), 0), ' CZK in ', year, ' could buy ', ROUND(AVG(industry_avg_payroll_value) / AVG(category_avg_value_price), 0), ' of kg/l of ', category_name, '.') AS comment_of_result
 FROM t_marcel_sufcak_project_SQL_primary_final
-WHERE year IN (2006, 2018)
-	AND cat_code IN (111301,114201)
-ORDER BY year, cat_code;
-
--- Employees from overall industries can buy (in 2006 and 2018):
-SELECT `year` 
-	,ROUND(AVG(ind_avg_payroll_value), 2) AS avg_payroll_all_industries
-	,ind_pay_unit 
-	,category_name 
-	,cat_code 
-	,cat_avg_value_price
-	,cat_price_unit 
-	,ROUND(AVG(ind_avg_payroll_value) / AVG(cat_avg_value_price), 0) AS available_entity
-	,CONCAT('A person with an average salary of ', ROUND(AVG(ind_avg_payroll_value), 0), ' CZK in ', year, ' could buy ', ROUND(AVG(ind_avg_payroll_value) / AVG(cat_avg_value_price), 0), ' of kg/l of ', category_name, '.') AS comment_of_result
-FROM t_marcel_sufcak_project_SQL_primary_final
-WHERE year IN (2006, 2018)
-	AND cat_code IN (111301,114201)
-GROUP BY year, cat_code
-ORDER BY year, cat_code;
+JOIN min_max_years m
+WHERE year IN (m.min_year, m.max_year)
+	AND category_code IN (111301,114201)
+GROUP BY year, category_code
+ORDER BY year, category_code;
