162aaf45d3b5cef30167cf5fdf981a9acd7d6aa0
Marcel Sufcak <153240524+mac3k@users.noreply.github.com>
2024-01-24 10:19:30 +0100

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -2,24 +2,24 @@
 
 -- Create view of percent. change:
 CREATE VIEW v_yearly_pct_price_change AS (
-SELECT a.year
-	,a.category_name 
-	,a.cat_code 
-	,a.cat_avg_value_price 
-	,a.cat_price_unit
-	,COALESCE(b.cat_avg_value_price,'N/A') AS previous_price
-	,ROUND((1-(b.cat_avg_value_price/a.cat_avg_value_price)), 3)*100 AS yearly_pct_price_change
+SELECT a.year,
+	a.category_name, 
+	a.category_code, 
+	a.category_avg_value_price, 
+	a.category_price_unit,
+	COALESCE(b.category_avg_value_price,'N/A') AS previous_price,
+	ROUND((1-(b.category_avg_value_price/a.category_avg_value_price)), 3)*100 AS yearly_pct_price_change
 FROM t_marcel_sufcak_project_SQL_primary_final a
 LEFT JOIN t_marcel_sufcak_project_sql_primary_final b
-	ON a.cat_code = b.cat_code
+	ON a.category_code = b.category_code
 	AND a.year = b.year + 1
-GROUP BY a.cat_code, a.year
+GROUP BY a.category_code, a.year
 );
-
+      
 -- To find out the food category name with the lowest price change during the period (2006 - 2018):
-SELECT category_name
-	,cat_code
-	,ROUND(AVG(yearly_pct_price_change), 2) AS avg_price_change
+SELECT category_name,
+	category_code,
+	ROUND(AVG(yearly_pct_price_change), 2) AS avg_price_change
 FROM v_yearly_pct_price_change
-GROUP BY cat_code
+GROUP BY category_code
 ORDER BY avg_price_change ASC;
