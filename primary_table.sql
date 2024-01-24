50085d5301c667ac739344e50724ddc596b27f63
Marcel Sufcak <153240524+mac3k@users.noreply.github.com>
2024-01-24 10:11:28 +0100

    Update primary_table.sql

--- primary_table.sql
+++ primary_table.sql
@@ -1,33 +1,24 @@
 -- discord name: marcel.suf
 -- primary table script
 CREATE TABLE IF NOT EXISTS t_marcel_sufcak_project_SQL_primary_final AS
-SELECT year
-	,industry_name
-	,industry_code
-	,ind_avg_payroll_value
-	,'CZK / month' AS ind_pay_unit
-	,category_name
-	,cat_code
-	,cat_avg_value_price
-	,CONCAT('CZK / ', cat_price_quantity, ' ', cat_unit) AS cat_price_unit
-FROM (  
-		SELECT YEAR(date_from) AS year
-	        ,category_code AS cat_code
-	        ,cpc.name AS category_name
-	        ,ROUND(AVG(value), 2) AS cat_avg_value_price
-	        ,cpc.price_value AS cat_price_quantity
-	        ,cpc.price_unit AS cat_unit
+WITH growceries AS (  
+		SELECT YEAR(date_from) AS year,
+	        category_code AS category_code,
+	        cpc.name AS category_name,
+	        ROUND (AVG (value), 2) AS category_avg_value_price,
+	        cpc.price_value AS category_price_quantity,
+	        cpc.price_unit AS category_unit
    		FROM czechia_price cpr
     	LEFT JOIN
        		czechia_price_category cpc ON cpr.category_code = cpc.code
    		GROUP BY
    			year, category_code
-		) a
- JOIN (
-    	SELECT payroll_year
-	        ,industry_branch_code AS industry_code
-	        ,cpib.name AS industry_name
-	        ,ROUND(AVG(value), 2) AS ind_avg_payroll_value
+		),
+payrolls AS (
+    	SELECT payroll_year,
+	        industry_branch_code AS industry_code,
+	        cpib.name AS industry_name,
+	        ROUND (AVG (value), 2) AS industry_avg_payroll_value
 	     FROM
 	        czechia_payroll cp
 	    LEFT JOIN
@@ -37,8 +28,16 @@ FROM (
 	        AND industry_branch_code IS NOT NULL
 	        AND cp.value_type_code = 5958
 	    GROUP BY payroll_year, industry_branch_code
-		) b 
+		)
+SELECT year,
+	industry_name,
+	industry_code,
+	industry_avg_payroll_value,
+	'CZK / month' AS industry_pay_unit,
+	category_name,
+	category_code,
+	category_avg_value_price,
+	CONCAT ('CZK / ', category_price_quantity, ' ', category_unit) AS category_price_unit
+FROM growceries a
+JOIN payrolls b
 	ON a.year = b.payroll_year;
-
-SELECT *
-FROM t_marcel_sufcak_project_SQL_primary_final;
